4c236cd69cba9f066894d400fa3205457f74fbf5
Santosh Raut <rautsantoshtukaram@gmail.com>
2024-08-27 19:28:49 +0530

    fix color change

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     access_token: "G_q2O7K3ncLB58WBPhAXbe_GfnOjQaRP",
     apiUrl: 'https://rk.raegesports.com',
-    //  apiUrl: 'http://0.0.0.0:8055'
+    // apiUrl: 'http://0.0.0.0:8055'
 };
